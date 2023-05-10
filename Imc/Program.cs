8e61497c316275ff864f47c397f8aad3ac7b8f23
Marcelo Cesar <marcelocesar9812@gmail.com>
2023-05-10 16:01:17 -0300

    first commmit

---
+++ Imc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+double peso, altura, imc;
+Console.WriteLine("Entra com o valor do peso:");
+peso = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+Console.WriteLine("Entra com o valor da altura:");
+altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+imc = peso / (altura * altura);
+
+if (imc <=18.5)
+    Console.WriteLine("magreza");
+
+if (imc >= 18.5 && imc <= 24.9)
+    Console.WriteLine("Normal");
+
+if (imc >= 25 && imc <= 29.9)
+    Console.WriteLine("Sobrepeso");
+
+if (imc >= 30 && imc <= 39.9)
+    Console.WriteLine("obesidade");
+
+if (imc >= 40.0)
+    Console.WriteLine("obesidade grave");
+
+Console.WriteLine("Seu imc é " + imc.ToString("F2"), CultureInfo.InvariantCulture);
